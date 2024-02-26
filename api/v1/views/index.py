aae78c380405d893104096b4df07477b9260987e
AnneMbulwa <annembulwa602@gmail.com>
2024-02-26 16:27:35 +0300

    jsonify

---
+++ api/v1/views/index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from flask import jsonify
+from app.v1.views import app_views
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route("/status", strict_slashes=False)
+def get_status():
+    """return json message of status"""
+    return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def get_stats():
+    """endpoint that retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
